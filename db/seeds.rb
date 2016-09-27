c681f653c771d9c6fdfccc34c9f4c5518901cbcf
melmar12 <melmar12@live.com>
2016-09-27 16:45:18 +0000

    seedin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Car.create(model:"Honda", year: 2020, description: 'this car is nice', color: 'blue', price: 38_500, use: "suburb")
+Car.create(model:"Toyota", year: 2016, description: 'this car is nice', color: 'red', price: 38_500, use: "suburb")
+Car.create(model:"Nissan", year: 2011, description: 'this car is cool', color: 'black', price: 38_500, use: "city")
+Car.create(model:"Toyota", year: 2013, description: 'this car is nice', color: 'green', price: 38_500, use: "suburb")
